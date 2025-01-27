96fed0e07d32bbd87e6b6692f301252aa78406fe
Buzz-Lightyear <m.srinivas1729@gmail.com>
2025-01-27 15:26:50 -0500

    Adding char class support

--- integration-tests.nix
+++ integration-tests.nix
@@ -1,8 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs }:
 let
   lib = pkgs.lib;
   globset = import ./. { inherit lib; };
-  fs = lib.fileset;
   testRoot = ./test-data;
 
   normalizeFileset = fileset:
@@ -11,21 +10,31 @@ let
       (lib.fileset.toList fileset));
 
   runTest = name: result: expected:
-    pkgs.writeScript "test-${name}" ''
-      #!/usr/bin/env bash
-      echo "Testing ${name}..."
-      expected='${builtins.toJSON expected}'
-      result='${builtins.toJSON result}'
-      if [ "$result" = "$expected" ]; then
-        echo "PASS: ${name}"
-        exit 0
-      else
-        echo "FAIL: ${name}"
-        echo "Expected: $expected"
-        echo "Got: $result"
-        exit 1
-      fi
-    '';
+    pkgs.stdenv.mkDerivation {
+      name = "test-${name}";
+      src = null;
+      dontUnpack = true;
+      doCheck = true;
+      checkPhase = ''
+        #!/usr/bin/env bash
+        echo "Testing ${name}..."
+        expected='${builtins.toJSON expected}'
+        result='${builtins.toJSON result}'
+        if [ "$result" = "$expected" ]; then
+          echo "PASS: ${name}"
+          exit 0
+        else
+          echo "FAIL: ${name}"
+          echo "Expected: $expected"
+          echo "Got: $result"
+          exit 1
+        fi
+      '';
+
+      buildPhase = ''
+        touch $out
+      '';
+    };
 
   testCases = {
     testGoProject = runTest "globs all Go files"
@@ -118,13 +127,93 @@ let
         "home-manager/users/teto/sway.nix"
         "home-manager/users/teto/swaync.nix"
       ];
+
+    testCharClass = runTest "character class matching"
+      (normalizeFileset (globset.glob testRoot "src/[fl]*.c")) [
+        "src/foo*.c"
+        "src/foobar.c"
+        "src/lib.c"
+      ];
+
+    testCharClassWithEscaping = runTest "character class matching w/ escaping"
+      (normalizeFileset (globset.glob testRoot "src/[e-g]oo\\*.c"))
+      [ "src/foo*.c" ];
+
+    testCharClassWithEscaping2 = runTest "character class matching w/ escaping 2"
+      (normalizeFileset (globset.glob testRoot "src/[e-g]oo\\-.[oc]"))
+      [ "src/foo-.o" ];
+
+    testCharClassWithEscapingInsideClass =
+      runTest "character class matching w/ escaping inside class"
+      (normalizeFileset (globset.glob testRoot "src/[e-g]oo[\\[\\-\\]\\*].[oc]")) [
+        "src/foo*.c"
+        "src/foo-.o"
+        "src/foo[.o"
+        "src/foo].o"
+      ];
+
+    testMultipleCharClassWithEscaping =
+      runTest "multiple character class matching w/ escaping"
+      (normalizeFileset (globset.glob testRoot "src/[e-g][^n][n-q]\\*.c"))
+      [ "src/foo*.c" ];
+
+    testCharRange = runTest "character range matching"
+      (normalizeFileset (globset.glob testRoot "**/[a-m]*.py"))
+      [ "scripts/main.py" ];
+
+    testNegatedClass = runTest "negated character class"
+      (normalizeFileset (globset.glob testRoot "src/[^t]*.c")) [
+        "src/foo*.c"
+        "src/foobar.c"
+        "src/lib.c"
+        "src/main.c"
+      ];
+
+    testNegatedClassMultiple = runTest "negated character class multiple"
+      (normalizeFileset (globset.glob testRoot "src/[^lt]*.c")) [
+        "src/foo*.c"
+        "src/foobar.c"
+        "src/main.c"
+      ];
+
+    testNegatedClassAlt = runTest "negated character class with !"
+      (normalizeFileset (globset.glob testRoot "src/[!t]*.c")) [
+        "src/foo*.c"
+        "src/foobar.c"
+        "src/lib.c"
+        "src/main.c"
+      ];
+
+    testCompoundClass = runTest "compound character class patterns"
+      (normalizeFileset (globset.glob testRoot "**/*.[ch]")) [
+        "src/foo*.c"
+        "src/foobar.c"
+        "src/lib.c"
+        "src/lib.h"
+        "src/main.c"
+        "src/test/test_main.c"
+      ];
+
+    testClassWithGlobs = runTest "Pass multiple ranges with globs"
+      (normalizeFileset
+        (globset.globs testRoot [ "**/*[m-o].py" "**/*[r-t].py" ])) [
+          "scripts/main.py"
+          "scripts/utils.py"
+        ];
+
+    testClassWithMixed = runTest "mixed character class with range and literals"
+      (normalizeFileset (globset.glob testRoot "**/ma[h-j]n.py"))
+      [ "scripts/main.py" ];
+
+    testEmptyCharClass = runTest "empty char class"
+      (normalizeFileset (globset.glob testRoot "src/[]*.c"))
+      [ ];
   };
 
-  runAllTests =
-    pkgs.runCommand "run-all-tests" { nativeBuildInputs = [ pkgs.bash ]; } ''
-      ${builtins.concatStringsSep "\n" (builtins.attrValues testCases)}
-      mkdir -p $out
-      echo "All tests passed!" > $out/result
-    '';
-
-in { inherit runAllTests; }
+in pkgs.runCommand "run-all-tests" {
+  nativeBuildInputs = [ pkgs.bash ];
+  buildInputs = builtins.attrValues testCases;
+} ''
+  mkdir -p $out
+  echo "All tests passed!" > $out/result
+''
--- internal/tests.nix
+++ internal/tests.nix
@@ -31,6 +31,106 @@ let
       );
 
 in {
+  parseCharClass = mkSuite {
+    testNameFn = testCase: ''parseCharClass "${testCase.input}" ${toString testCase.startIdx}'';
+    valueFn = testCase: internal.parseCharClass testCase.input testCase.startIdx;
+    tests = [
+      {
+        input = "[abc]def";
+        startIdx = 0;
+        expected = {
+          content = "abc";
+          endIdx = 4;
+          isNegated = false;
+        };
+      }
+      {
+        input = "x[^0-9]";
+        startIdx = 1;
+        expected = {
+          content = "0-9";
+          endIdx = 6;
+          isNegated = true;
+        };
+      }
+      {
+        input = "[a\\]b]";
+        startIdx = 0;
+        expected = {
+          content = "a\\]b";
+          endIdx = 5;
+          isNegated = false;
+        };
+      }
+      {
+        input = "[\\^abc]";
+        startIdx = 0;
+        expected = {
+          content = "\\^abc";
+          endIdx = 6;
+          isNegated = false;
+        };
+      }
+      {
+        input = "[!a-z]123";
+        startIdx = 0;
+        expected = {
+          content = "a-z";
+          endIdx = 5;
+          isNegated = true;
+        };
+      }
+      {
+        input = "[^abc]";
+        startIdx = 0;
+        expected = {
+          content = "abc";
+          endIdx = 5;
+          isNegated = true;
+        };
+      }
+      {
+        input = "[abc";
+        startIdx = 0;
+        expected = {
+          content = "abc";
+          endIdx = -1;
+          isNegated = false;
+        };
+      }
+    ];
+  };
+
+  matchesCharClass = mkSuite {
+    testNameFn = testCase: ''matchesCharClass "${testCase.class}" "${testCase.char}"'';
+    valueFn = testCase: internal.matchesCharClass testCase.class testCase.char;
+    tests = [
+      {class = "abc"; char = "b"; expected = true;}
+      {class = "abc"; char = "d"; expected = false;}
+      {class = "a-c"; char = "c"; expected = true;}
+      {class = "a-c"; char = "d"; expected = false;}
+      {class = "^0-9"; char = "a"; expected = true;}
+      {class = "^0-9"; char = "9"; expected = false;}
+      {class = "!e-g"; char = "d"; expected = true;}
+      {class = "!e-g"; char = "f"; expected = false;}
+      {class = "^abc"; char = "d"; expected = true;}
+      {class = "!abc"; char = "c"; expected = false;}
+    ];
+  };
+
+  inCharRange = mkSuite {
+    testNameFn = testCase: ''inCharRange "${testCase.start}" "${testCase.end}" "${testCase.char}"'';
+    valueFn = testCase: internal.inCharRange testCase.start testCase.end testCase.char;
+    tests = [
+      {start = "a"; end = "c"; char = "b"; expected = true;}
+      {start = "a"; end = "c"; char = "B"; expected = false;}
+      {start = "0"; end = "9"; char = "5"; expected = true;}
+      {start = "3"; end = "6"; char = "7"; expected = false;}
+      {start = "3"; end = "6"; char = "6"; expected = true;}
+      {start = "3"; end = "6"; char = "3"; expected = true;}
+    ];
+  };
+
   firstUnescapedMeta = mkSuite {
     testNameFn = testCase: ''firstUnescapedMeta "${testCase.str}"'';
     valueFn = testCase: internal.firstUnescapedMeta testCase.str;
@@ -92,6 +192,93 @@ in {
       { pattern = "a/**/c"; path = "a/b/c"; expected = true; }
       { pattern = "a/**/d"; path = "a/b/c/d"; expected = true; }
       { pattern = "a/\\**"; path = "a/b/c"; expected = false; }
+
+      # Character Class
+      {
+        pattern = "[abc]";
+        path = "b";
+        expected = true;
+      }
+      {
+        pattern = "[abc]";
+        path = "d";
+        expected = false;
+      }
+      {
+        pattern = "[a-z]";
+        path = "m";
+        expected = true;
+      }
+      {
+        pattern = "[a-z]";
+        path = "3";
+        expected = false;
+      }
+      {
+        pattern = "[^abc]";
+        path = "d";
+        expected = true;
+      }
+      {
+        pattern = "[^abc]";
+        path = "b";
+        expected = false;
+      }
+      {
+        pattern = "[!0-9]";
+        path = "a";
+        expected = true;
+      }
+      {
+        pattern = "[!0-9]";
+        path = "5";
+        expected = false;
+      }
+      {
+        pattern = "foo[abc].txt";
+        path = "fooa.txt";
+        expected = true;
+      }
+      {
+        pattern = "[a-z]/**/*.txt";
+        path = "d/foo/bar.txt";
+        expected = true;
+      }
+      {
+        pattern = "*.[ch]";
+        path = "foo.c";
+        expected = true;
+      }
+      {
+        pattern = "*.[ch]";
+        path = "foo.o";
+        expected = false;
+      }
+      {
+        pattern = "[^a-e][!p-r]o.c";
+        path = "foo.c";
+        expected = true;
+      }
+      {
+        pattern = "f[\\-].c";
+        path = "f-.c";
+        expected = true;
+      }
+      {
+        pattern = "f[\\[\\]].c";
+        path = "f[.c";
+        expected = true;
+      }
+      {
+        pattern = "f[\\[\\]].c";
+        path = "f].c";
+        expected = true;
+      }
+      {
+        pattern = "[ef]\\*.c";
+        path = "f*.c";
+        expected = true;
+      }
     ];
   };
 }
